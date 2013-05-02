7bd3d4054e94cb3fd894ac43ce0bbc35d8744e34
MattP1994 <matthewprenger@gmail.com>
2013-05-01 21:53:10 -0500

    Added a Teleport All Command

--- common/com/matthewprenger/servermod/command/CommandTpAll.java
+++ common/com/matthewprenger/servermod/command/CommandTpAll.java
@@ -1,15 +1,40 @@
 package com.matthewprenger.servermod.command;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 
 public class CommandTpAll extends Command {
     public CommandTpAll(){
         super("tpall");
     }
     
+    @SuppressWarnings("rawtypes")
     @Override
     public void processCommand(ICommandSender var1, String[] var2) {
-        //TODO Add command to teleport all players
+        
+        double x, y, z;
+                
+        x = var1.getPlayerCoordinates().posX;
+        y = var1.getPlayerCoordinates().posY;
+        z = var1.getPlayerCoordinates().posZ;
+        
+        /*TODO Add multiple dimention support*/
+        
+        ArrayList playerlist = (ArrayList) MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+        Iterator iterator = playerlist.iterator();
+        
+        int teleported = 0;
+        while(iterator.hasNext()){
+            EntityPlayerMP player = (EntityPlayerMP) iterator.next();
+            player.setPositionAndUpdate(x, y, z);
+            teleported++;
+        }
+        
+        notifyAdmins(var1, "Teleported all " + teleported + " players to X: " + x + " Y: " + y + " Z: " + z);
     }
     
     
